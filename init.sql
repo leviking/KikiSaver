64d63db5a2734b6f963d05998d8701613a658383
Eliot Sanford <eliot587@gmail.com>
2020-03-16 20:54:04 -0500

    update: created users table with eight columns (#19)
    fixes #7

--- init.sql
+++ init.sql
@@ -1,11 +1,24 @@
 drop database kiki_saver;
 create database kiki_saver;
 
-CREATE USER 'admin'@'localhost' IDENTIFIED BY 'abc123';
+create USER 'admin'@'localhost' IDENTIFIED BY 'abc123';
 grant all PRIVILEGES on *.* to 'admin'@'localhost';
 
 use kiki_saver;
 
+create table users (
+    id tinyint auto_increment,
+    username varchar(20),
+    password varhar(20),
+    first_name varchar(20),
+    last_name varchar(30),
+    phone varchar(10),
+    created_at datetime,
+    deleted_at datetime,
+    constraint user_id primary key (id),
+    constraint username unique(username),
+    constraint email unique(email)
+);
 create table attendance(
     id int auto_increment,
     user_id int,
