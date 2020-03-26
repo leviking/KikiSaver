cfd786e9b3cd66cfe623e7bcf43767fbca5d34e3
Eliot Sanford <eliot587@gmail.com>
2020-03-25 19:54:40 -0500

    update: create user directory at signup closes #103

--- api/sign-up.js
+++ api/sign-up.js
@@ -1,15 +1,23 @@
+const fs = require('fs')
 const path = require('path')
 const { con } = require('./db')
     //Sign up stuff
 const sendSignup = (req, res) => res.sendFile(path.join(__dirname + '/../public/signup.html'))
 const getSignupQuery = (username, password, firstName, lastName, phone) => `insert into users(username, password, first_name, last_name, phone, created_at) values ('${username}', '${password}','${firstName}', '${lastName}','${phone}', now())`
+
+const createUserDir = (username) => {
+    fs.mkdir(path.join(__dirname + `/../public/selfies/${username}`), {recursive: true}, (err) => console.log('ok'))}
+
 const createUser = (req, res) => {
     con.query(getSignupQuery(req.body.username, req.body.password, req.body.firstName, req.body.lastName, req.body.phone), function(error, results, fields) {
         if (error) {
             res.status(400).send(`${JSON.stringify(error)}.`);
             return;
         };
-        if (results) res.redirect('/');
+        if (results) {
+            createUserDir(req.body.username)
+            res.redirect('/');
+        }
     })
 }
 
