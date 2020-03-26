1c18bee8562537c72ed37da11c958c40e3ce021d
Garrick Crouch <services@localpathcomputing.com>
2020-03-25 20:40:07 -0500

    add function to protect admin route,set http head

--- api/login.js
+++ api/login.js
@@ -12,6 +12,9 @@ const getLoginQuery = (username, password) => {
 const getAttendanceQuery = (id, ip, gps) => {
     return `insert into attendance (user_id, created_at, gps, ip) values ('${id}', now(), '${gps}', '${ip}')`
 }
+const getAdminQuery = (username, password) => {
+    return `SELECT is_admin FROM users WHERE username='${username}' AND password='${password}'`
+}
 const logAttendance = (id, ip, gps, res) => {
     con.query(getAttendanceQuery(id, ip, gps), (err, results, fields) => {
         if (err) {
@@ -30,18 +33,32 @@ const login = (req, res) => {
             res.send('no results')
         } else {
             (results[0].is_admin == true) ?
-                loginEmitter.emit('onAdmin', res):
+            loginEmitter.emit('onAdmin', req, res):
                 loginEmitter.emit('onLogin', results[0].id, req.ip, req.body.gps, res);
         }
     })
 }
 
+const sendAdmin = (req, res) => {
+    if (con.query(getAdminQuery(req.headers.msuser, req.headers.mspass), (err, results, fields) => {
+            if (err) console.log(err)
+            if (!results.length || results[0].is_admin == false) {
+                console.log({ results: results, status: 'not admin' });
+                res.status(401).send('Not admin')
+                return
+            }
+            res.redirect('/admin')
+                //res.sendFile(path.join(__dirname + '/../public/admin.html'))
+        }))
+        return
+}
+
 loginEmitter.on('onLogin', logAttendance)
-loginEmitter.on('onAdmin', (res) => {
+loginEmitter.on('onAdmin', (req, res) => {
     //add auth
-    res.sendFile(path.join(__dirname + '/../public/admin.html'))
+    res.set({ msuser: req.body.username, mspass: req.body.password }).sendFile(path.join(__dirname + '/../public/admin.html'))
 })
 loginEmitter.on('loginSuccess', (res) => {
     res.sendFile(path.join(__dirname + '/../public/success.html'))
 })
-module.exports = { login, sendLogin }
+module.exports = { login, sendLogin, sendAdmin }
