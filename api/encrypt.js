095b3152f0591d91ed2d394be54be8d03f8b915c
leviking <levi@mscode.dev>
2020-04-28 18:46:30 -0500

    fix: add callback that works to createUser

--- api/encrypt.js
+++ api/encrypt.js
@@ -4,14 +4,14 @@ const EventEmitter = require('events');
 // const encryptEmitter = new EventEmitter();
 
 
-const encrypt = (password, encryptEmitter) => { 
+const encrypt = (password, callback) => { 
   bcrypt.hash(password, saltRounds, function(err, hash) {
     if(err) console.log(err);
     
     
     console.log('encrypt ', password, hash);
     
-    encryptEmitter.emit('encrypted', hash)
+    callback(hash)
   });
 }
 
