7bc2015830f1ea1c83f4e00878eea3ab09d8fd64
Garrick Crouch <services@localpathcomputing.com>
2020-05-01 09:29:52 -0500

    fix: authed user update pw

--- api/changePassword.js
+++ api/changePassword.js
@@ -30,40 +30,39 @@ const changePassword = (req, res) => {
 
     } else {
 
-        oldPassword = Buffer.from(req.body.oldPassword, 'base64').toString()
-        
-        const returnHash = (req) => {
-            con.query(getHashedPass(req.body.username), (err, results, fields) => {
+        const decodedOldPass =  Buffer.from(req.body.oldPassword, 'base64').toString()
+        const compareWithPass = (loginPass, callback) => (dbPass) => {        
+            compare(loginPass, dbPass, callback)
+        }
+        const getPassword = (username, callback) => {
+            con.query(`SELECT password FROM users WHERE username = '${username}'`,
+                (err, results) => {            
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        callback(results[0].password)
+                    }
+                }
+            )
+        }
+        const updateNoMagicLinkPass = (username) => (newPassword) => {
+            con.query(updatePassQuery(newPassword, username), (err, results, fields) => {
                 if (err) {
-                    res.status(400).send('err')
                     console.log(err)
+                    res.status(400).send('err')
                     return
-                } else if (results.rowsMatched === 0) {
-                    res.status(403).send('Your old password is incorrect!')
-                    return
-                } else {
-                    console.log(results)
-                    return results[0].password
-                }
-            })
-
-        }
-        const updatePasswordWithNewPassword = () => {
-            con.query(noMagicLinkQuery(password, req.body.username), (error, results, fields) => {
-                if (error) {
-                    res.status(400).send('No match');
-                    console.log(error)
-                    return;
-                } else if (results) {
-                    res.status(200).send('Ok');
+                } else if (results.affectedRows === 1) {
+                    console.log('success');
+                    res.status(200).send('Password Updated')                 
                 }
             })
         }
-            compare(oldPassword, returnHash(req), passCheck)
-        encrypt(
-            Buffer.from(req.body.newPassword, 'base64').toString(), 
-            updatePasswordWithNewPassword()
-        )
+        const handleLogin = (results) => {
+            results ?
+            encrypt(decodedOldPass, updateNoMagicLinkPass(req.body.username)) :
+            res.status(401).send('Password incorrect')
+        }
+        getPassword(req.body.username, compareWithPass(decodedOldPass, handleLogin))
      }
 }
 
@@ -72,14 +71,11 @@ const magicLinkQuery = (password, magicLink) => {
         SELECT user_id FROM user_resets WHERE reset_key = '${magicLink}' AND deleted_at IS null
         )`
 }
-const noMagicLinkQuery = (password, username) => {
+const updatePassQuery = (password, username) => {
     return `UPDATE users SET password = '${password}' WHERE username = '${username}'`
 }
 const destroyMagicQuery = resetLink => {
     return `UPDATE user_resets SET deleted_at = now() WHERE reset_key = '${resetLink}'`
-}
-const getHashedPass = (username) => {
-    return `SELECT password from users WHERE username = '${username}'`
 }
 
 const hasMagicLink = req => !!req.body.magic
