fb3f8298e6403b13a1f0ad17ab6ab5272049eac0
Garrick Crouch <services@localpathcomputing.com>
2020-04-30 20:45:32 -0500

    fix: nonauth user forgot pass

--- api/changePassword.js
+++ api/changePassword.js
@@ -7,74 +7,62 @@ const { encrypt, compare } = require('./encrypt')
 const changePassword = (req, res) => {
     
     if (hasMagicLink(req)) {
-        con.query(magicLinkQuery(req.body.newPassword, req.body.magic), (err, results, fields) => {
-            if (err) {
-                console.log(err)
-                res.status(400).send('err')
-            } else {
-                if (results.affectedRows == 0) {
-                    res.status(403).send('noKeyMatch')
+
+        const decodedNewPass = Buffer.from(req.body.newPassword, 'base64').toString()
+
+        const updateMagicLinkPass = (magic) => (newPassword) => {
+            con.query(magicLinkQuery(newPassword, magic), (err, results, fields) => {
+                if (err) {
+                    console.log(err)
+                    res.status(400).send('err')
                     return
+                } else {
+                    if (results.affectedRows == 0) {
+                        res.status(403).send('noKeyMatch')
+                        return
+                    }
+                    resetEmitter.emit('successfulReset', magic)
+                    res.status(200).send('ok')
                 }
-                resetEmitter.emit('successfulReset', req.body.magic)
-                res.status(200).send('ok')
-                console.log(results)
-            }
-        })
+            })
+        }
+        encrypt(decodedNewPass, updateMagicLinkPass(req.body.magic))
 
     } else {
-        const getPassword = (username, callback) => {
-            con.query(`select password from users where username='${username}'`,
-                (err, results) => {
-                    console.log(results[0].password);
-                    
-                    if(err) {console.log(err)
-                    }else{
-                        callback(results[0].password)
-                    }
-                }
-            )
-        }
-        
-        const getUser = (username, callback) => {
-            con.query(`select * from users where username='${username}'`,
-            (err, results) => {
-                if(err) {
-                    console.log(err)
-                } else {
-                    callback(results[0])
-                }
-            }
-            )
-        }
 
-        const compareWithPass = (oldPassword, callback) => (dbPass) => {
-            console.log(dbPass);
-            
-            compare(oldPassword, dbPass, callback)
+        oldPassword = Buffer.from(req.body.oldPassword, 'base64').toString()
+        
+        const returnHash = (req) => {
+            con.query(getHashedPass(req.body.username), (err, results, fields) => {
+                if (err) {
+                    res.status(400).send('err')
+                    console.log(err)
+                    return
+                } else if (results.rowsMatched === 0) {
+                    res.status(403).send('Your old password is incorrect!')
+                    return
+                } else {
+                    console.log(results)
+                    return results[0].password
+                }
+            })
+
         }
-        const decodedOldPass =  Buffer.from(req.body.oldPassword, 'base64').toString()
-        const updatePasswordWithNewPassword = (res, req) => (password) => {
+        const updatePasswordWithNewPassword = () => {
             con.query(noMagicLinkQuery(password, req.body.username), (error, results, fields) => {
                 if (error) {
-                    res.status(400).send(`${JSON.stringify(error)}.`);
+                    res.status(400).send('No match');
+                    console.log(error)
                     return;
-                };
-                if (results) {
-                        //res.redirect('/');
+                } else if (results) {
                     res.status(200).send('Ok');
                 }
             })
         }
-        const handleChangePassword = (results) => {
-            results ?
-            getUser(req.body.username) :
-            res.status(401).send('Password incorrect')
-        }
-        getPassword(req.body.username, compareWithPass(decodedOldPass, handleChangePassword))
+            compare(oldPassword, returnHash(req), passCheck)
         encrypt(
             Buffer.from(req.body.newPassword, 'base64').toString(), 
-            updatePasswordWithNewPassword(res,req)
+            updatePasswordWithNewPassword()
         )
      }
 }
@@ -85,11 +73,13 @@ const magicLinkQuery = (password, magicLink) => {
         )`
 }
 const noMagicLinkQuery = (password, username) => {
-
     return `UPDATE users SET password = '${password}' WHERE username = '${username}'`
 }
 const destroyMagicQuery = resetLink => {
     return `UPDATE user_resets SET deleted_at = now() WHERE reset_key = '${resetLink}'`
+}
+const getHashedPass = (username) => {
+    return `SELECT password from users WHERE username = '${username}'`
 }
 
 const hasMagicLink = req => !!req.body.magic
--- api/encrypt.js
+++ api/encrypt.js
@@ -1,11 +1,9 @@
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const EventEmitter = require('events');
 
 const encrypt = (password, callback) => { 
   bcrypt.hash(password, saltRounds, function(err, hash) {
     if(err) console.log(err);
-    console.log(hash)
     callback(hash)
   });
 }
